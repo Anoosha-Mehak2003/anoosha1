212a754a00e906ad378d5f1d88253a69fbbdcf10
Anoosha-Mehak2003 <wubat2002@gmail.com>
2024-06-11 22:57:51 +0500

    Add files via upload
    this is my first project

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package practice;
+//Anoosha mehak
+//21b-200-se
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, I'm Anoosha Mehak!");
+
+
+	}
+
+}
